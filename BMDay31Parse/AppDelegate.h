0c73a35dc1b1ba6a891a3b24c99ec32465f5cc9a
kunwardeep <kunwardeep.gill@gmail.com>
2015-05-11 10:46:01 -0400

    Initial Commit

---
+++ BMDay31Parse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMDay31Parse
+//
+//  Created by Kunwardeep Gill on 2015-05-11.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMDay31Parse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMDay31Parse
+//
+//  Created by Kunwardeep Gill on 2015-05-11.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
